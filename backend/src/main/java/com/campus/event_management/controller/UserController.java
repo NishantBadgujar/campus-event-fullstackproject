08824eaca297d6ea9fdfd551f76cbce35bf7f833
Nishant Badgujar <nishantvijayb@gmail.com>
2025-07-28 23:51:53 +0530

    add service and controller

---
+++ backend/src/main/java/com/campus/event_management/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.campus.event_management.controller;
+
+import com.campus.event_management.entity.User;
+import com.campus.event_management.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService service;
+
+
+    @PostMapping("create")
+    public User create(@RequestBody User user){
+        return service.createUser(user);
+    }
+
+    @GetMapping("getAll")
+    public List<User> getAll(){
+        return service.getAllUser();
+    }
+
+    @GetMapping("get/{id}")
+    public User getUser(@PathVariable long id){
+        return service.getUserById(id);
+    }
+
+    @PutMapping("update/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable long id){
+        return service.updateUser(user,id);
+    }
+}
